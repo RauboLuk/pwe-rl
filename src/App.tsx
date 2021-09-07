14d5dffd0abaa7f71e4806e8cacc54b3e847fe71
RauboLuk <rauboluk@gmail.com>
2021-09-07 23:30:16 +0200

    feat: add task bar - mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
 import "./App.css";
 
-import Button from "@material-ui/core/Button";
+import { useState } from "react";
+import Box from "@material-ui/core/Box";
+import AddTask from "./components/AddTask";
+
+interface Todo {
+  id: number;
+  text: string;
+  isDone: boolean;
+}
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>(
+    Array(3_000)
+      .fill(0)
+      .map((_, i) => ({
+        id: Math.random(),
+        text: String(i),
+        isDone: false,
+      }))
+  );
+
+  const addTodo = (text: string): void => {
+    setTodos((todos) => [
+      ...todos,
+      {
+        id: Math.random(),
+        text,
+        isDone: false,
+      },
+    ]);
+  };
+
   return (
-    <div className="App">
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-    </div>
+    <Box>
+      <AddTask addTodo={addTodo} />
+      <ul>
+        {todos?.map(({ id, text }) => (
+          <li key={id}>{`${text}`}</li>
+        ))}
+      </ul>
+    </Box>
   );
 }
 
---
+++ src/components/AddTask.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+import AddIcon from "@material-ui/icons/Add";
+import IconButton from "@material-ui/core/IconButton";
+import InputBase from "@material-ui/core/InputBase";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+    mxWidth: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+}));
+
+const AddTask = ({ addTodo }: { addTodo: (text: string) => void }) => {
+  const classes = useStyles();
+  const [task, setTask] = useState<string>("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTask(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    addTodo(task);
+    setTask("");
+  };
+
+  return (
+    <Paper component="form" onSubmit={handleSubmit} className={classes.root}>
+      <InputBase
+        value={task}
+        onChange={handleChange}
+        placeholder="Add task"
+        inputProps={{ "aria-label": "add task" }}
+        className={classes.input}
+      />
+      <IconButton type="submit" aria-label="add" className={classes.iconButton}>
+        <AddIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default AddTask;
